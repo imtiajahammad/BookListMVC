f34739446b68d370f80f161c964cd00fa5e596ed
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2021-04-04 00:24:03 +0600

    Index view added and constructor added with db argument

---
+++ BookListMVC/Controllers/BooksController.cs
@@ -0,0 +1,22 @@
+﻿using BookListMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListMVC.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        public BooksController(ApplicationDbContext applicationDbContext)
+        {
+            this._applicationDbContext = applicationDbContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
